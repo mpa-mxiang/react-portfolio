ab7c5d9d28179590610005b4dc1364d9c1549011
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2024-02-26 23:19:32 -0500

    start with sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,13 @@
 import './index.scss';
 
 const Layout = () => {
-    return <>Hello</>
+    return 
+    <div className='nav-bar'>  
+      <Link className='logo' to='/'>
+        <img src={LogoS} alt="logo" />
+        <img src={LogoSubtitle} alt="slobodan" />
+      </Link>
+    </div>>
 }
 
 export default Layout;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,8 @@
+import './index.scss'
+import Sidebar from '../Sidebar';
+const Sidebar = () => {
+    return
+    <>
+    <Sidebar />
+    </>
+}

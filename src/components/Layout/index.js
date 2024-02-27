f8c2f2d8363764f8fae6a03f330985b769b22c0c
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2024-02-26 23:26:58 -0500

    fix layout with installing react-router-dom

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.scss';
 
 const Layout = () => {
-    return 
-    <div className='nav-bar'>  
+  return (
+    <div className='nav-bar'>
       <Link className='logo' to='/'>
         <h1>Jiachun</h1>
       </Link>
-    </div>>
+    </div>
+  );
 }
 
 export default Layout;

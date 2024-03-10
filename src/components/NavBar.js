00d863b8f8e3ca59d0126abcd15587bae4173fcd
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2024-03-09 23:48:25 -0500

    fix useEffect

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,19 +4,19 @@ export const Navbar = () => {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
 
-    useEffect{()=> {
-        const onScroll = () => {
-            if(window.scrollY > 50) {
-                setScrolled(true);
-            }
-            else{
-                setScrolled(false);
-            }
-        }
+    useEffect(() => {
+      const onScroll = () => {
+          if (window.scrollY > 50) {
+              setScrolled(true);
+          } else {
+              setScrolled(false);
+          }
+      }
 
-        window.addEventListener("scroll", onScroll);
-        return () => window.removeEventListener("scroll", onScroll);
-    }}
+      window.addEventListener("scroll", onScroll);
+
+      return () => window.removeEventListener("scroll", onScroll);
+  }, []);
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>

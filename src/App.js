75d1f2d5e244d0cd874a4a7efdcf634553002330
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2024-03-22 23:13:44 -0400

    import Projects component in app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }

5ad11be6100a1c37269dd4af0f0ab8e4d0e72136
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2024-03-30 22:44:01 -0400

    change formDetail for email and edit typo for value

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -30,10 +30,10 @@ export const Contact = () => {
                         <form>
                             <Row>
                                 <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.name} placeholder="Name" onChange={(e) => onFormUpdate('name', e.target.valiue)} />
+                                    <input type="text" value={formDetails.name} placeholder="Name" onChange={(e) => onFormUpdate('name', e.target.value)} />
                                 </Col>
                                 <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.name} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.valiue)} />
+                                    <input type="text" value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)} />
                                 </Col>
                             </Row>
                         </form>

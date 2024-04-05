80c3dfbb30181d8693613bea5cda80572c896dc5
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2024-04-04 23:13:38 -0400

    change postUrl setting

--- src/components/MailChimpForm.js
+++ src/components/MailChimpForm.js
@@ -1,7 +1,7 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 
 export const MailchimpForm = () => {
-    const postUrl = '${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U?u=${process.env.REACT_APP_MAILCHIMP_ID';
+    const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
 
     return (
         <>

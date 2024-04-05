dddfb079ee6d01a19e2e2c25e30393711cc17628
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2024-04-04 23:44:51 -0400

    fix mailchimp server listen port

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,7 +24,7 @@ export const Contact = () => {
         setButtonText('Sending...');
     
         try {
-            let response = await fetch("http://localhost:3000/contact", {
+            let response = await fetch("http://localhost:5000/contact", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json;charset=utf-8",

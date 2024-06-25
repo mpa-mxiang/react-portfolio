531098fc22007e5cba89c412704bc8e849a6a15c
Jiachun Xiang <jcxiang1230@gmail.com>
2024-06-24 23:45:02 -0400

    add black bg color for main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen flex-col items-center justify-between p-24">
+    <main className="relative bg-black min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1>Hello Profolio!</h1>
         </div>

f5416a8ca283848a1da396d35b7acaff2ba5f504
Jiachun Xiang <jcxiang1230@gmail.com>
2024-06-26 00:00:02 -0400

    add more basic styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="relative bg-black min-h-screen flex items-center justify-center p-24">
+    <main className="relative bg-black min-h-screen flex-col mx-auto items-center justify-center p-24 sm:px-10 px-5">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1 className="text-white">Hello Profolio!</h1>
         </div>

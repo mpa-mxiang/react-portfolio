4b55ebbe4a2bd8f7b070b4ce573c4edf554a7f30
Jiachun Xiang <jcxiang1230@gmail.com>
2024-06-25 23:25:55 -0400

    add text color to be able to see

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="relative bg-black min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>Hello Profolio!</h1>
+        <h1 className="text-white">Hello Profolio!</h1>
         </div>
     </main>
   );

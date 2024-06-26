39ded3045fab5a8d2baec9e6923199a980bf0043
Jiachun Xiang <jcxiang1230@gmail.com>
2024-06-25 23:40:18 -0400

    change main styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black min-h-screen flex-col items-center justify-between p-24">
+    <main className="relative bg-black min-h-screen flex items-center justify-center p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1 className="text-white">Hello Profolio!</h1>
         </div>

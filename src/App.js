e2b716f125cb1572acdb49bdf157a9013dbe5d30
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-05 12:35:14 -0400

    import react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './containers/home';
+import About from './containers/about';
+import Portfolio from './containers/portfolio';
+import Contact from './containers/contact';
+import Resume from './containers/resume';
+import Skills from './containers/skills';
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route index path='/' element={<Home />} />
+        <Route index path='/about' element={<About />} />
+        <Route index path='/resume' element={<Resume />} />
+        <Route index path='/skills' element={<Skills />} />
+        <Route index path='/portfolio' element={<Portfolio />} />
+        <Route index path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   );
 }

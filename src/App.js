aa309b566f3ba301b95aa39eeb37eae49b23cdc2
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-09 23:52:30 -0400

    import particles in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Particles from "react-tsparticles";
+import { loadFull } from 'tsparticles';
 import Home from './containers/home';
 import About from './containers/about';
 import Portfolio from './containers/portfolio';
@@ -7,9 +9,18 @@ import Contact from './containers/contact';
 import Resume from './containers/resume';
 import Skills from './containers/skills';
 import Navbar from './components/navBar';
+import particles from './utils.js/particles';
 function App() {
+
+  const handleInit = async (main) => {
+    await loadFull(main)
+  }
+
   return (
     <div className="App">
+
+    <Particles id="particles" options={particles} init={handleInit} />
+
       <Navbar />
       <Routes>
         <Route index path='/' element={<Home />} />

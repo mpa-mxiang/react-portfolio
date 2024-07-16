ea4bb9cfd1a2a495b93b11b07a2e0a89261dbdd5
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-15 22:26:13 -0400

    add particle render condition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Particles from "react-tsparticles";
 import { loadFull } from 'tsparticles';
 import Home from './containers/home';
@@ -12,14 +12,18 @@ import Navbar from './components/navBar';
 import particles from './utils.js/particles';
 function App() {
 
+  const renderParticle = location.pathname === "/";
+
   const handleInit = async (main) => {
     await loadFull(main)
   }
 
   return (
     <div className="App">
-
-    <Particles id="particles" options={particles} init={handleInit} />
+      {
+        renderParticle && <Particles id="particles" options={particles} init={handleInit} />
+      }
+    
 
       <Navbar />
       <Routes>

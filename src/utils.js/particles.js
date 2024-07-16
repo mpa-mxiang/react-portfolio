724ee43622b945748b37f3ef1568a47c1b6dbdf0
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-15 20:29:30 -0400

    fix eslint warning

--- src/utils.js/particles.js
+++ src/utils.js/particles.js
@@ -1,72 +1,74 @@
-export default  {
-    background: {
-        color: {
+const particlesConfig = {
+  background: {
+      color: {
           value: "#1d1d1d",
-        },
       },
-      fpsLimit: 120,
-      interactivity: {
-        events: {
+  },
+  fpsLimit: 120,
+  interactivity: {
+      events: {
           onClick: {
-            enable: true,
-            mode: "push",
+              enable: true,
+              mode: "push",
           },
           onHover: {
-            enable: true,
-            mode: "repulse",
+              enable: true,
+              mode: "repulse",
           },
           resize: true,
-        },
-        modes: {
+      },
+      modes: {
           push: {
-            quantity: 4,
+              quantity: 4,
           },
           repulse: {
-            distance: 200,
-            duration: 0.4,
+              distance: 200,
+              duration: 0.4,
           },
-        },
       },
-      particles: {
-        color: {
+  },
+  particles: {
+      color: {
           value: "var(--seleted-theme-main-color)",
-        },
-        links: {
+      },
+      links: {
           color: "#ffffff",
           distance: 150,
           enable: true,
           opacity: 0.5,
           width: 1,
-        },
-        collisions: {
-            enable: true,
-        },
-        move: {
+      },
+      collisions: {
+          enable: true,
+      },
+      move: {
           direction: "none",
           enable: true,
           outModes: {
-            default: "bounce",
+              default: "bounce",
           },
           random: false,
           speed: 6,
           straight: false,
-        },
-        number: {
+      },
+      number: {
           density: {
-            enable: true,
-            area: 800,
+              enable: true,
+              area: 800,
           },
           value: 80,
-        },
-        opacity: {
-          value: 0.5,
-        },
-        shape: {
-          type: "circle",
-        },
-        size: {
-          value: { min: 1, max: 5 },
-        },
       },
-      detectRetina: true,
-}
+      opacity: {
+          value: 0.5,
+      },
+      shape: {
+          type: "circle",
+      },
+      size: {
+          value: { min: 1, max: 5 },
+      },
+  },
+  detectRetina: true,
+};
+
+export default particlesConfig;

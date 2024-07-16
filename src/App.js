1384c0ea6dc02b5e96bd97684c992a7e3f903f9a
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-15 23:28:02 -0400

    fix useLocation issue

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+import "./App.scss";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Particles from "react-tsparticles";
-import { loadFull } from 'tsparticles';
-import Home from './containers/home';
-import About from './containers/about';
-import Portfolio from './containers/portfolio';
-import Contact from './containers/contact';
-import Resume from './containers/resume';
-import Skills from './containers/skills';
-import Navbar from './components/navBar';
-import particles from './utils.js/particles';
+import { loadFull } from "tsparticles";
+import Home from "./containers/home";
+import About from "./containers/about";
+import Resume from "./containers/resume";
+import Skills from "./containers/skills";
+import Portfolio from "./containers/portfolio";
+import Contact from "./containers/contact";
+import Navbar from "./components/navBar";
+import particles from "./utils.js/particles";
+
 function App() {
+  const location = useLocation();
+  console.log(location);
 
   const handleInit = async (main) => {
-    await loadFull(main)
-  }
+    await loadFull(main);
+  };
+
+  const renderParticleJsInHomePage = location.pathname === "/";
 
   return (
     <div className="App">
-      <Particles id="particles" options={particles} init={handleInit} />
+      {/* particles js */}
 
+      {renderParticleJsInHomePage && (
+        <Particles id="particles" options={particles} init={handleInit} />
+      )}
 
+      {/* navbar */}
       <Navbar />
-      <div className='App__main-content'>
 
+      {/* main page content */}
+      <div className="App__main-page-content">
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       </div>
 
-
-      <Routes>
-        <Route index path='/' element={<Home />} />
-        <Route index path='/about' element={<About />} />
-        <Route index path='/resume' element={<Resume />} />
-        <Route index path='/skills' element={<Skills />} />
-        <Route index path='/portfolio' element={<Portfolio />} />
-        <Route index path='/contact' element={<Contact />} />
-      </Routes>
     </div>
   );
 }

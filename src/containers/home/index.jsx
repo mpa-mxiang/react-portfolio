b5da0e51c750a69ed112d2e32c4808a470bbea24
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-17 23:51:14 -0400

    add onclick for navigate to contact page

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./styles.scss";
 const Home = () => {
+
+  const navigate = useNavigate();
+
+  const handleNav = () => {
+    
+  }
   return (
-    <section id="Home" className='home'>
-      <div className='home__text-wrapper'>
+    <section id="Home" className="home">
+      <div className="home__text-wrapper">
         <h1>
           Hello, I'm Jiachun
           <br />
           Developer
         </h1>
       </div>
-      <div className='home__contact-me'>
-        <button>Hire me</button>
+      <div className="home__contact-me">
+        <button onClick={handleNav}>Hire me</button>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

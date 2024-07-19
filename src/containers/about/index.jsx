0664399cc5aebe300bf0ee5a66c88f157098521e
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-18 23:59:46 -0400

    fix about page import errors

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PageHeader from '../../components/pageHeader';
 import { BsInfoCircleFill } from "react-icons/bs";
+
 const About = () => {
   return (
     <section id="about" className='about'>
       <PageHeader
-      headerText = "About Me"
-      icon={BsInfoCircleFill size={40}} />
+        headerText="About Me"
+        icon={<BsInfoCircleFill size={40} />} 
+      />
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;

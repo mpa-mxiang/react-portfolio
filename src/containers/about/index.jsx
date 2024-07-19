b457e85e5bf1bccb2b53716670942ac9c7507303
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-18 23:58:28 -0400

    import pageHeader and react bs icon

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-
+import React from 'react';
+import PageHeader from '../../components/pageHeader';
+import { BsInfoCircleFill } from "react-icons/bs";
 const About = () => {
   return (
     <section id="about" className='about'>
       <PageHeader
       headerText = "About Me"
-      icon={BsInfoCircleFill size={40}}>
+      icon={BsInfoCircleFill size={40}} />
     </section>
   )
 }

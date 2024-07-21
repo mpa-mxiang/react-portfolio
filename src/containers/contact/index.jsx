607de129a3a6170dca6fe7a0d6eb0a7da9105062
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-21 00:01:21 -0400

    add personDetails

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 import PageHeader from '../../components/pageHeader';
 import { BsInfoCircleFill } from "react-icons/bs";
 
+const personDetails = [
+  {
+    label: "name",
+    value: "Jiachun Xiang",
+  },
+  {
+
+    label: "Address",
+    value: "Canada",
+  },
+  {
+    label: "Email",
+    value: "jcxiang1230@gmail.com",
+  },
+];
 const Contact = () => {
   return (
     <section id="contact" className='contact'>

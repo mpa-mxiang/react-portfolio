f6e32bc093bc42437d3dcaa9636e7f2c28fd3288
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-29 23:55:00 -0400

    add animate for new about summary div

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import PageHeader from "../../components/pageHeader";
 import { BsInfoCircleFill } from "react-icons/bs";
+import { Animate } from "react-simple-animate";
 
 const About = () => {
   return (
     <section id="about" className="about">
       <PageHeader headerText="About Me" icon={<BsInfoCircleFill size={40} />} />
       <div className="about__content">
+      <Animate
+      play duration={1.5}
+      delay={1}
+      start={{transform : 'translateY(550px)'}}
+      end={{transform : 'translateY(0px)'}}
+      >
         <h3>Developer</h3>
         <p>{jobSummary}</p>
       </div>
+      </Animate>
     </section>
   );
 };

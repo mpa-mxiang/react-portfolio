854da2c8a27f0250808ec642ab2713400e645dd6
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-29 23:56:22 -0400

    import styles file and fix import animate issue

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -2,22 +2,25 @@ import React from "react";
 import PageHeader from "../../components/pageHeader";
 import { BsInfoCircleFill } from "react-icons/bs";
 import { Animate } from "react-simple-animate";
-
+import './styles.scss';
 const About = () => {
   return (
     <section id="about" className="about">
       <PageHeader headerText="About Me" icon={<BsInfoCircleFill size={40} />} />
       <div className="about__content">
-      <Animate
-      play duration={1.5}
-      delay={1}
-      start={{transform : 'translateY(550px)'}}
-      end={{transform : 'translateY(0px)'}}
-      >
-        <h3>Developer</h3>
-        <p>{jobSummary}</p>
+        <Animate
+          play
+          duration={1.5}
+          delay={1}
+          start={{ transform: 'translateY(550px)' }}
+          end={{ transform: 'translateY(0px)' }}
+        >
+          <div>
+            <h3>Developer</h3>
+            <p>{jobSummary}</p>
+          </div>
+        </Animate>
       </div>
-      </Animate>
     </section>
   );
 };

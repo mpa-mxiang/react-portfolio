628cbf8b237690a710c59b45f1205f32c9be3aa1
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-29 23:53:51 -0400

    add a div for about summary

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import PageHeader from '../../components/pageHeader';
+import React from "react";
+import PageHeader from "../../components/pageHeader";
 import { BsInfoCircleFill } from "react-icons/bs";
 
 const About = () => {
   return (
-    <section id="about" className='about'>
-      <PageHeader
-        headerText="About Me"
-        icon={<BsInfoCircleFill size={40} />} 
-      />
+    <section id="about" className="about">
+      <PageHeader headerText="About Me" icon={<BsInfoCircleFill size={40} />} />
+      <div className="about__content">
+        <h3>Developer</h3>
+        <p>{jobSummary}</p>
+      </div>
     </section>
   );
 };

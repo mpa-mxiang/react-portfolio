3b90a9b51641f230ce938e2e0d30cd1c3a912a80
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-30 23:54:39 -0400

    complete person info part with mapping of personalDetails

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -3,6 +3,21 @@ import PageHeader from "../../components/pageHeader";
 import { BsInfoCircleFill } from "react-icons/bs";
 import { Animate } from "react-simple-animate";
 import './styles.scss';
+const personDetails = [
+  {
+    label: "name",
+    value: "Jiachun Xiang",
+  },
+  {
+
+    label: "Address",
+    value: "Canada",
+  },
+  {
+    label: "Email",
+    value: "jcxiang1230@gmail.com",
+  },
+];
 const About = () => {
   return (
     <section id="about" className="about">
@@ -20,13 +35,25 @@ const About = () => {
             <p>{jobSummary}</p>
           </div>
         </Animate>
-
+        <Animate
+          play
+          duration={1.5}
+          delay={1}
+          start={{ transform: 'translateY(500px)' }}
+          end={{ transform: 'translateY(0px)' }}
+        >
         <h3>Personal Info</h3>
         <ul>
           {
-            personal
+            personDetails.map((item, i)=>(
+              <li key={i}>
+                <span className="title">{item.label}</span>
+                <span className="value">{item.value}</span>
+              </li>
+            ))
           }
         </ul>
+        </Animate>
       </div>
     </section>
   );

696a1a809ddd5b4d17603a35d91ab0bbcc00a3cb
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-31 00:04:54 -0400

    add some styles for about

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import PageHeader from "../../components/pageHeader";
 import { BsInfoCircleFill } from "react-icons/bs";
 import { Animate } from "react-simple-animate";
-import './styles.scss';
+import "./styles.scss";
 const personDetails = [
   {
     label: "name",
     value: "Jiachun Xiang",
   },
   {
-
     label: "Address",
     value: "Canada",
   },
@@ -23,37 +22,39 @@ const About = () => {
     <section id="about" className="about">
       <PageHeader headerText="About Me" icon={<BsInfoCircleFill size={40} />} />
       <div className="about__content">
-        <Animate
-          play
-          duration={1.5}
-          delay={1}
-          start={{ transform: 'translateY(550px)' }}
-          end={{ transform: 'translateY(0px)' }}
-        >
-          <div>
-            <h3>Developer</h3>
-            <p>{jobSummary}</p>
-          </div>
-        </Animate>
-        <Animate
-          play
-          duration={1.5}
-          delay={1}
-          start={{ transform: 'translateY(500px)' }}
-          end={{ transform: 'translateY(0px)' }}
-        >
-        <h3>Personal Info</h3>
-        <ul>
-          {
-            personDetails.map((item, i)=>(
-              <li key={i}>
-                <span className="title">{item.label}</span>
-                <span className="value">{item.value}</span>
-              </li>
-            ))
-          }
-        </ul>
-        </Animate>
+        <div className="about__content__personalWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{ transform: "translateX(550px)" }}
+            end={{ transform: "translateX(0px)" }}
+          >
+            <div>
+              <h3>Developer</h3>
+              <p>{jobSummary}</p>
+            </div>
+          </Animate>
+        </div>
+        <div className="about__content__infoWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{ transform: "translateY(500px)" }}
+            end={{ transform: "translateY(0px)" }}
+          >
+            <h3 className="personalInfoText">Personal Info</h3>
+            <ul>
+              {personDetails.map((item, i) => (
+                <li key={i}>
+                  <span className="title">{item.label}</span>
+                  <span className="value">{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </Animate>
+        </div>
       </div>
     </section>
   );

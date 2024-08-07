b83d9be5f58f27f2abb55ba7582c15bfbebed803
Jiachun Xiang <jcxiang1230@gmail.com>
2024-08-06 22:02:03 -0400

    delete three sections

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import Home from "./containers/home";
 import About from "./containers/about";
-import Resume from "./containers/resume";
 import Skills from "./containers/skills";
-import Portfolio from "./containers/portfolio";
-import Contact from "./containers/contact";
 import Navbar from "./components/navBar";
 import particles from "./utils.js/particles";
 
@@ -34,10 +31,7 @@ function App() {
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
         <Route path="/skills" element={<Skills />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
       </div>
 

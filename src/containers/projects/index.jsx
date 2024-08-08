b3284a7eaa9bb148fd3f4d5d284849e153096b81
Jiachun Xiang <jcxiang1230@gmail.com>
2024-08-08 00:05:41 -0400

    import img

--- src/containers/projects/index.jsx
+++ src/containers/projects/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PageHeader from '../../components/pageHeader';
 import { BsInfoCircleFill } from "react-icons/bs";
-
+import Img from "../../images/special-work.png";
 const Projects = () => {
   return (
     <section id="projects" className='projects'>

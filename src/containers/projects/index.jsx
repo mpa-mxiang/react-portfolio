a3cd290b89c4a3246e9f0df7b024e572f82d6544
Jiachun Xiang <jcxiang1230@gmail.com>
2024-08-08 23:45:14 -0400

    create filiter data var to filter project data by id

--- src/containers/projects/index.jsx
+++ src/containers/projects/index.jsx
@@ -11,36 +11,56 @@ const projectData = [
   },
   {
     id: 3,
-    name: "project1",
+    name: "project2",
     image: Img,
   },
   {
     id: 2,
-    name: "project1",
+    name: "project3",
     image: Img,
   },
   {
     id: 2,
-    name: "project1",
+    name: "project4",
     image: Img,
   },
   {
     id: 2,
-    name: "project1",
+    name: "project5",
     image: Img,
   },
 
   {
     id: 2,
-    name: "project1",
+    name: "project6",
     image: Img,
   },
   {
     id: 2,
-    name: "project1",
+    name: "project7",
     image: Img,
   },
 ];
+
+const filterData = [
+  {
+    filterId: 1,
+    label: "All",
+  },
+  {
+    filterId: 2,
+    label: "Front End",
+  },
+  {
+    filterId: 3,
+    label: "Back End",
+  },
+  {
+    filterId: 4,
+    label: "Full Stack",
+  },
+]
+
 const Projects = () => {
   return (
     <section id="projects" className="projects">

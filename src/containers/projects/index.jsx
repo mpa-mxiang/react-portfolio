7c7781bfc0794ca6b0339a298c6702a33e81ddbb
Jiachun Xiang <jcxiang1230@gmail.com>
2024-08-08 23:35:58 -0400

    create projectData variable to store projects for future ref

--- src/containers/projects/index.jsx
+++ src/containers/projects/index.jsx
@@ -1,16 +1,52 @@
-import React from 'react'
-import PageHeader from '../../components/pageHeader';
+import React from "react";
+import PageHeader from "../../components/pageHeader";
 import { BsInfoCircleFill } from "react-icons/bs";
 import Img from "../../images/special-work.png";
+
+const projectData = [
+  {
+    id: 2,
+    name: "project1",
+    image: Img,
+  },
+  {
+    id: 3,
+    name: "project1",
+    image: Img,
+  },
+  {
+    id: 2,
+    name: "project1",
+    image: Img,
+  },
+  {
+    id: 2,
+    name: "project1",
+    image: Img,
+  },
+  {
+    id: 2,
+    name: "project1",
+    image: Img,
+  },
+
+  {
+    id: 2,
+    name: "project1",
+    image: Img,
+  },
+  {
+    id: 2,
+    name: "project1",
+    image: Img,
+  },
+];
 const Projects = () => {
   return (
-    <section id="projects" className='projects'>
-      <PageHeader
-        headerText="Projects"
-        icon={<BsInfoCircleFill size={40} />} 
-      />
+    <section id="projects" className="projects">
+      <PageHeader headerText="Projects" icon={<BsInfoCircleFill size={40} />} />
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;

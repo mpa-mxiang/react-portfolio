14fd7b4875be54c9d8933513a1df644f94177727
Jiachun Xiang <jcxiang1230@gmail.com>
2024-08-16 23:12:00 -0400

    setup basic popup

--- src/containers/projects/index.jsx
+++ src/containers/projects/index.jsx
@@ -3,49 +3,50 @@ import PageHeader from "../../components/pageHeader";
 import { BsInfoCircleFill } from "react-icons/bs";
 import Img from "../../images/spcial-work.png";
 import "./styles.scss";
+import Popup from "../../components/popup/index";
 
 const projectData = [
   {
     id: 2,
     name: "project1",
     image: Img,
-    link: '',
+    link: "",
   },
   {
     id: 3,
     name: "project2",
     image: Img,
-    link: '',
+    link: "",
   },
   {
     id: 4,
     name: "project3",
     image: Img,
-    link: '',
+    link: "",
   },
   {
     id: 2,
     name: "project4",
     image: Img,
-    link: '',
+    link: "",
   },
   {
     id: 2,
     name: "project5",
     image: Img,
-    link: '',
+    link: "",
   },
   {
     id: 2,
     name: "project6",
     image: Img,
-    link: '',
+    link: "",
   },
   {
     id: 2,
     name: "project7",
     image: Img,
-    link: '',
+    link: "",
   },
 ];
 
@@ -71,6 +72,8 @@ const filterData = [
 const Projects = () => {
   const [filteredVal, setFilteredVal] = useState(1);
   const [hoverVal, setHoveredVal] = useState(null);
+  const [btnPopup, setBtnPopup] = useState(false);
+  const [activeProject, setActiveProject] = useState(null);
 
   function handleFilter(currentId) {
     setFilteredVal(currentId);
@@ -80,8 +83,10 @@ const Projects = () => {
     setHoveredVal(index);
   }
 
-  const filteredItems = filteredVal === 1 ? 
-    projectData : projectData.filter(item => item.id === filteredVal);
+  const filteredItems =
+    filteredVal === 1
+      ? projectData
+      : projectData.filter((item) => item.id === filteredVal);
 
   return (
     <section id="projects" className="projects">
@@ -89,7 +94,11 @@ const Projects = () => {
       <div className="projects__content">
         <ul className="projects__content__filter">
           {filterData.map((item) => (
-            <li className={item.filterId === filteredVal ? 'active': ''} onClick={() => handleFilter(item.filterId)} key={item.filterId}>
+            <li
+              className={item.filterId === filteredVal ? "active" : ""}
+              onClick={() => handleFilter(item.filterId)}
+              key={item.filterId}
+            >
               {item.label}
             </li>
           ))}
@@ -111,7 +120,14 @@ const Projects = () => {
                 <div className="overlay">
                   <div>
                     <p>{item.name}</p>
-                    <button>Visit</button>
+                    <button
+                      onClick={() => {
+                        setActiveProject(item);
+                        setBtnPopup(true);
+                      }}
+                    >
+                      Visit
+                    </button>
                   </div>
                 </div>
               )}
@@ -119,6 +135,15 @@ const Projects = () => {
           ))}
         </div>
       </div>
+
+      <Popup trigger={btnPopup} setTrigger={setBtnPopup}>
+        {activeProject && (
+          <div>
+            <h3>{activeProject.name}</h3>
+            <p>This is more information about {activeProject.name}.</p>
+          </div>
+        )}
+      </Popup>
     </section>
   );
 };

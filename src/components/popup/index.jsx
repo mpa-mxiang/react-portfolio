f6fe11caad14c4343ea0539c74a812f78b3cf9ac
Jiachun Xiang <jcxiang1230@gmail.com>
2024-08-17 00:10:50 -0400

    fix popup mobile view

--- src/components/popup/index.jsx
+++ src/components/popup/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "./index.scss";
+import "./styles.scss";
 
 function Popup(props) {
   return props.trigger ? (
     <div className="popup">
-      <div className="popup-inner">
-        <button className="close-btn" onClick={() => props.setTrigger(false)}>
+      <div className="popup__inner">
+        <button className="popup__inner__btn" onClick={() => props.setTrigger(false)}>
           X
         </button>
         {props.children}
